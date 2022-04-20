3a7e34483baf20e31fcb9b9e0634f35e48a22c0d
Firmin Launay <hey@firminlaunay.me>
2022-04-20 19:00:16 +0200

    Minor changes

--- main.cpp
+++ main.cpp
@@ -5,10 +5,9 @@ int main(int argc, const char** argv) {
     Shape3D shapeCube = cube();
     Shape3D shapeTetrahedron = tetrahedron();
 
-
-    if (argc != 2) {
+    if (argc != 2)
         throw std::runtime_error("Please specify the right number of parameters.");
-    }
+
     std::ifstream file = std::ifstream(argv[1]);
     if (!file.is_open()) throw std::runtime_error("Error while reading the file.");
     Model model = Model(std::move(file));
