116392e4efe9c03353ff66b8d8e4b3897628a6e2
Firmin Launay <hey@firminlaunay.me>
2022-06-06 14:32:07 +0200

    Add bash script to simplify parsing of database

---
+++ parse_database.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $# -eq 2 ]
+then
+    find "${2%/}/*/train/*.off" | xargs -I{} "./$1" {}
+else
+    >&2 echo "Usage: ./parse_database.sh [database_directory] [off_parser_executable]"
+fi
